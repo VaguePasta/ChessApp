a08efc99cec623d23cc31be7b864b034135cd9c8
VaguePasta <qphuc21158@gmail.com>
2024-09-07 17:37:37 +0700

    Added pinging to server.

--- src/connection/websocket.ts
+++ src/connection/websocket.ts
@@ -1,19 +1,73 @@
-import {GameServer, MatchMakingServer} from "../main";
-import {ConnectToLobby, Lobby, ProcessMatchmakingRequest} from "../match/matchmaking";
-
+import {WebSocketServer, WebSocket } from 'ws'
+import {ConnectToLobby, ProcessMatchmakingRequest} from "../match/matchmaking";
+export class CustomWebSocket extends WebSocket {
+    isAlive: boolean | undefined
+}
+export class CustomWebsocketServer extends WebSocketServer<typeof CustomWebSocket> {
+    constructor() {
+        super({
+            noServer: true,
+            WebSocket: CustomWebSocket,
+        })
+    }
+}
+export const GameServer = new CustomWebsocketServer()
+export const MatchMakingServer = new CustomWebsocketServer()
 export function ProcessUpgrades(request: any, socket: any, head: any): boolean {
     let requestData = request.url.slice(1).split("/")
     if (requestData[0] === "match") {
         MatchMakingServer.handleUpgrade(request, socket, head, (ws) => {
             ProcessMatchmakingRequest(ws)
+            MatchMakingServer.emit('connection', ws)
         })
         return true
     }
     else if (requestData[0] === "game") {
         GameServer.handleUpgrade(request, socket, head, (ws) => {
             ConnectToLobby(ws, requestData[1])
+            GameServer.emit('connection', ws)
         })
         return true
     }
     return false
 }
+
+GameServer.on('connection', (ws) => {
+    ws.isAlive = true
+    ws.on('pong', () => {
+        // @ts-ignore
+        this.isAlive = true
+    })
+})
+setInterval(() => {
+    GameServer.clients.forEach((ws) => {
+        if (!ws.isAlive) return ws.terminate()
+        ws.isAlive = false
+        ws.ping(null, undefined)
+    })
+}, 15000)
+setInterval(() => {
+    MatchMakingServer.clients.forEach((ws) => {
+        if (!ws.isAlive) return ws.terminate()
+        ws.isAlive = false
+        ws.ping(null, undefined)
+    })
+}, 20000)
+MatchMakingServer.on('connection', (ws) => {
+    ws.isAlive = true
+    ws.on('pong', () => {
+        ws.emit('alive', ws)
+    })
+    ws.on('alive', (ws) => {
+        ws.isAlive = true
+    })
+})
+GameServer.on('connection', (ws) => {
+    ws.isAlive = true
+    ws.on('pong', () => {
+        ws.emit('alive', ws)
+    })
+    ws.on('alive', (ws) => {
+        ws.isAlive = true
+    })
+})
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,8 @@ import express = require('express')
 import {StartChessEngine} from "./game/engine/engine";
 export const app = express()
 import cors from 'cors';
-import {WebSocketServer} from "ws";
 import http = require('http')
-import {ProcessUpgrades} from "./connection/websocket";
+import {CustomWebsocketServer, ProcessUpgrades} from "./connection/websocket";
 app.use([
     express.text({
         type: "text/plain",
@@ -22,12 +21,6 @@ server.listen(8080,() => {
     console.log("Starting chess engine....")
     StartChessEngine()
     console.log('Server started. Listening on 8080.')
-})
-export const GameServer = new WebSocketServer({
-    noServer: true,
-})
-export const MatchMakingServer = new WebSocketServer({
-    noServer: true,
 })
 server.on('upgrade', function upgrade(request, socket, head) {
     if (!ProcessUpgrades(request, socket, head))
