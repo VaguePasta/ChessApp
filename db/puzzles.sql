e9186fd4faf0b9c83961cb35b0bde5c98572ccac
VaguePasta <qphuc21158@gmail.com>
2024-11-14 08:59:40 +0700

    Update puzzles.sql

--- db/puzzles.sql
+++ db/puzzles.sql
@@ -2,7 +2,7 @@ create table if not exists puzzles (
 	puzzle_id int primary key generated always as identity,
 	fen text,
 	moves text,
-	rating smallint.
+	rating smallint,
 	rating_deviation smallint,
 	theme text
 )
